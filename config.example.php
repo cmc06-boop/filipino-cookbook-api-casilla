b66c92241205f2c8cba700dd4ecf6add02e9802f
cmc06-boop <casillacherrylyn1@gmail.com>
2026-07-28 20:47:35 +0800

    Update the dbname

--- config.example.php
+++ config.example.php
@@ -2,7 +2,7 @@
 <?php
 // Example configuration file - copy to config.php and fill in credentials
 $host = 'localhost';
-$dbname = 'filipino_cookbook_api';
+$dbname = 'filipino_cookbook_api_surname';
 $username = 'YOUR_DATABASE_USERNAME';
 $password = 'YOUR_DATABASE_PASSWORD';
 $apiToken = 'YOUR_API_TOKEN_HERE';
